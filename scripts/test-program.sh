93883051cfd18d241b3a8682127c1686655e538d
man0s <95379755+losman0s@users.noreply.github.com>
2024-07-07 14:17:46 +0000

    commit

--- scripts/test-program.sh
+++ scripts/test-program.sh
@@ -4,13 +4,7 @@ set -e
 ROOT=$(git rev-parse --show-toplevel)
 cd $ROOT
 
-program_lib_name=$1
-loglevel=$2
-
-if [ -z "$program_lib_name" ]; then
-    echo "Usage: $0 <program_lib_name>"
-    exit 1
-fi
+loglevel=$1
 
 if [ "$loglevel" == "--sane" ]; then
     loglevel=warn
@@ -20,12 +14,8 @@ else
     nocapture="--nocapture"
 fi
 
-if [ "$program_lib_name" == "all" ]; then
-    package_filter=""
-else 
-    package_filter="--package $program_lib_name"
-fi
+cd $ROOT/programs/marginfi
 
-cmd="RUST_LOG=solana_runtime::message_processor::stable_log=$loglevel cargo nextest run --no-fail-fast $package_filter --features=test,test-bpf $nocapture"
+cmd="SBF_OUT_DIR=$ROOT/target/deploy RUST_LOG=solana_runtime::message_processor::stable_log=$loglevel cargo-test-bpf --features=test-bpf -- $nocapture"
 echo "Running: $cmd"
 eval "$cmd"
